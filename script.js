e69cd81cbf2844ca01d155717d62a36c61fb518b
Stephanie smith <smithse4@gmail.com>
2020-03-06 18:56:44 -0500

    added conditional for user to enter at least one type of characters

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function generatePassword() {
   console.log("Generating my password!");
 }
 
-// Input variables:
-var confirmSpecialCharacters;
-var confirmNumbers;
-var confirmLowerLetters;
-var confirmUpperLetters;
+// // Input variables:
+// var confirmSpecialCharacters;
+// var confirmNumbers;
+// var confirmLowerLetters;
+// var confirmUpperLetters;
 
 // Possible password character options:
 var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "|",];
@@ -24,37 +24,33 @@ var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 // Prompt the user for password length:
   var passwordLength = prompt("How many characters would you like in your password? The number of characters in your password must be between 8 and 128.");
 
-    if(parseInt(passwordLength) >= 8 && parseInt(passwordLength) <=128){
-      alert("Your passwaord will have " + parseInt(passwordLength) + " characters.")
+  var userPWLength = parseInt(passwordLength)
+
+// Confirm password is a number between 8 and 128:
+    if(userPWLength >= 8 && userPWLength <=128){
+      alert("Your passwaord will have " + userPWLength + " characters.")
     } else {
       alert("You must enter a number to generate password length. Refresh page to try again.")
     }
 
-   
-
-// Make sure they entered a number:
-  // if(userInput === "Left" || userInput === "Right"){
-  //   console.log(userInput);
-   
-  // }else{
-  //    promptUser("You must enter a number to generate password length.")
-  // }
-
-// Check to see if the user actually entered a length
-
-
 // Boolean prompts to confirm if the following 4 character types are permitted in users password:
   // 1. Special characters
-  var specialCharacters = confirm("Are special characters okay in your password?");
+  var confirmSpecialCharacters = confirm("Are special characters okay in your password?");
 
   // .2 Numbers?
-  var numCharacters = confirm("Are numeric characters okay in your password?");
+  var confirmNumberCharacters = confirm("Are numeric characters okay in your password?");
 
   // 3. Lower case?
-  var lowerCharacters = confirm("Are lowercase characters okay in your password?");
+  var confirmLowerCharacters = confirm("Are lowercase characters okay in your password?");
 
   // 4. Upper case?
-  var upperCharacters = confirm("Are uppercase characters okay in your password?");
+  var confirmUpperCharacters = confirm("Are uppercase characters okay in your password?");
+
+
+// If user answers false for all 4 character sets:
+if (!confirmSpecialCharacters && !confirmNumberCharacters && !confirmLowerCharacters && !confirmUpperCharacters) {
+  alert("You must choose at least one character type.");
+}
 
     // Accomodate answers!generate password based on user specifications
     // NOTE: user specifications come from confirms
@@ -64,27 +60,5 @@ var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
       // }
       // problem: how to go get special charactors - SEE LINK IN README
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Conditionals for password:
 
-  passwordText.value = password;
-
-}
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// // NOTES
-// function generatePassword() {
-//   var length = 8,
-//       charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-//       retVal = "";
-//   for (var i = 0, n = charset.length; i < length; ++i) {
-//       retVal += charset.charAt(Math.floor(Math.random() * n));
-//   }
-//   return retVal;
-// }
